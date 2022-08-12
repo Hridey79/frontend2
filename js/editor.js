4d658bae90a2a7b9ec25107919de54f6ca8a64a7
HrideyVerma79 <82920883+HrideyVerma79@users.noreply.github.com>
2022-08-12 07:31:20 +0530

    changes to UI

--- js/editor.js
+++ js/editor.js
@@ -1,12 +1,27 @@
 const title=document.getElementById('titlearea');
 const text=document.getElementById('dataarea');
-const output=document.getElementById('blog2');
 const publish=document.getElementById('publish');
-
+const blogtable=document.getElementById("blogtable");
 
 function fun(){
-    console.log(text.value);
-    output.innerHTML=text.value;
+    
+    const ndiv=document.createElement("div");
+    ndiv.classList.add('blog');
+    const nh3=document.createElement('h3');
+    nh3.classList.add('titlearea');
+    nh3.innerHTML=title.value;
+    const ndata=document.createElement("div");
+    ndata.innerHTML=text.value;
+    ndata.classList.add('dataarea');
+    
+    ndiv.appendChild(nh3);
+    ndiv.appendChild(ndata);
+    console.log(ndiv);
+    console.log(blogtable)
+
+    // blogtable.appendChild(ndiv)
+
+
 }
 
 publish.addEventListener('click',fun);
